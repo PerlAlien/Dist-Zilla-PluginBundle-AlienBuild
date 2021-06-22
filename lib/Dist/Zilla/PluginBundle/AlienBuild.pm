c85f339d72837fca2465d050845988b2f551ec28
Graham Ollis <plicease@cpan.org>
2021-06-21 20:01:57 -0600

    initial structure based on @Starter

--- lib/Dist/Zilla/PluginBundle/AlienBuild.pm
+++ lib/Dist/Zilla/PluginBundle/AlienBuild.pm
@@ -1,14 +1,95 @@
 use strict;
 use warnings;
 use 5.020;
-use experimental qw( postderef );
 
 package Dist::Zilla::PluginBundle::AlienBuild {
 
   use Moose;
+  use experimental qw( postderef signatures );
+  with 'Dist::Zilla::Role::PluginBundle::Easy',
+    'Dist::Zilla::Role::PluginBundle::Config::Slicer',
+    'Dist::Zilla::Role::PluginBundle::PluginRemover';
+  use Ref::Util qw( is_plain_coderef );
   use namespace::autoclean;
 
   # ABSTRACT: A minimal Dist::Zilla plugin bundle for Aliens
+
+  my %revisions = (
+    1 => [
+      'GatherDir',
+      'MetaYAML',
+      'MetaJSON',
+      'License',
+      'Pod2Readme',
+      'PodSyntaxTests',
+      'Test::ReportPrereqs',
+      [ 'Test::Compile' => { xt_mode => 1 }],
+      sub { $_[0]->pluginset_installer },
+      'Manifest',
+      ['PruneFiles' => { filename => ['README.pod'] }],
+      'ManifestSkip',
+      'RunExtraTests',
+      # plugin_release_management?
+      'TestRelease',
+      'ConfirmRelease',
+      # plugin_releasers
+      ['MetaNoIndex' => { directory => [qw( t xt inc share eg examples )] } ],
+      # metaprovides
+      # execdir
+      ['Prereqs' => { -phase => 'runtime', perl => '5.008004' } ],
+    ],
+  );
+
+  my %allowed_installers = map { $_ => 1 } qw( MakeMaker MakeMaker::Awesome );
+
+=head1 PROPERTIES
+
+=head2 installer
+
+=cut
+
+  has installer => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub { $_[0]->payload->{installer} // 'MakeMaker' },
+  );
+
+=head2 revision
+
+=cut
+
+  has revision => (
+    is => 'ro',
+    lazy => 1,
+    default => sub { $_[0]->payload->{revision} // 1 },
+  );
+
+  sub pluginset_installer ($self) {
+    my $installer = $self->installer;
+    die "Unsupported installer $installer\n"
+      unless $allowed_installers{$installer};
+    return "$installer";
+  }
+
+  sub configure ($self) {
+    my $name = $self->name;
+    my $revision = $self->revision;
+    die "Unknown [$name] revision specified: $revision"
+      unless exists $revisions{$revision};
+    my @plugins = $revisions{$revision}->@*;
+
+    foreach my $plugin (@plugins)
+    {
+      if(is_plain_coderef $plugin)
+      {
+        $self->add_plugins($plugin->($self));
+      }
+      else
+      {
+        $self->add_plugins($plugin);
+      }
+    }
+  }
 
   __PACKAGE__->meta->make_immutable;
 }
--- t/dist_zilla_pluginbundle_alienbuild.t
+++ t/dist_zilla_pluginbundle_alienbuild.t
@@ -1,8 +1,71 @@
 use Test2::V0 -no_srand => 1;
 use Dist::Zilla::PluginBundle::AlienBuild;
+use Path::Tiny qw( path );
+use Test::DZil;
 
-ok 1, 'todo';
+subtest 'revision = 1' => sub {
+
+  my $tzil = Builder->from_config(
+    { dist_root => 'does-not-exist' },
+    {
+      add_files => {
+        path('source', 'dist.ini') => simple_ini({ version => '0.01' },
+          ['@AlienBuild' => { revision => 1 }],
+        ),
+        path('source', 'lib', 'Alien', 'libfoo.pm') => "package Alien::libfoo;\nour \$VERSION = '0.01';\n1",
+        path('source', '.git', 'this-should-get-pruned') => "",
+        path('source', 'README.pod') => "prune me\n",
+        path('source', 'Changes') => "{{\$NEXT}}\n  - Initial version\n",
+      },
+    },
+  );
+
+  $tzil->build;
+
+  my $build_dir = path($tzil->tempdir)->child('build');
+  my @found_files;
+  my $iter = $build_dir->iterator({ recurse => 1 });
+  while (my $path = $iter->())
+  {
+    push @found_files, $path->relative($build_dir)->stringify if -f $path;
+  }
+
+  is [sort @found_files], [qw(
+    Changes
+    LICENSE
+    MANIFEST
+    META.json
+    META.yml
+    Makefile.PL
+    README
+    dist.ini
+    lib/Alien/libfoo.pm
+    t/00-report-prereqs.dd
+    t/00-report-prereqs.t
+    xt/author/00-compile.t
+    xt/author/pod-syntax.t
+  )], 'built the correct files';
+
+  my $meta = $tzil->distmeta;
+
+  is(
+    $meta,
+    hash {
+      field version => '0.01';
+      field prereqs => hash {
+        field runtime => hash {
+          field requires => hash {
+            field perl => '5.008004';
+            etc;
+          };
+          etc;
+        };
+        etc;
+      };
+      etc;
+    },
+  );
+
+};
 
 done_testing;
-
-
