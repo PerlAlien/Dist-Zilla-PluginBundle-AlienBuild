22e4af407b64c9d75ada46b3d1c94528873d5b60
Graham Ollis <plicease@cpan.org>
2021-06-22 09:12:44 -0600

    better documentation

--- lib/Dist/Zilla/MintingProfile/AlienBuild.pm
+++ lib/Dist/Zilla/MintingProfile/AlienBuild.pm
@@ -14,3 +14,32 @@ package Dist::Zilla::MintingProfile::AlienBuild {
 }
 
 1;
+
+=head1 SYNOPSIS
+
+ dzil new -P AlienBuild Alien::libfoo
+
+=head1 DESCRIPTION
+
+This is a L<Dist::Zilla> minting profile for creating L<Alien> distributions
+based on the L<Alien::Build> framework.  It uses a reasonable template and the
+L<[@AlienBuild]|Dist::Zilla::PluginBundle::AlienBuild> bundle, which is itself
+based on the L<[@Starter]|Dist::Zilla::PluginBundle::Starter> bundle.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Alien>
+
+=item L<Alien::Build>
+
+=item L<alienfile>
+
+=item L<[@AlienBuild]|Dist::Zilla::PluginBundle::AlienBuild>
+
+=item L<[AlienBuild]|Dist::Zilla::Plugin::AlienBuild>
+
+=back
+
+=cut
